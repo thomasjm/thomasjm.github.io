3699e55bfcbbdde6c5cf301ccd5f03a57ee194dc
Tom McLaughlin <tom@codedown.io>
2023-04-02 13:13:18 -0700

    ruby -> 3.1, jekyll -> 4.2.2

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem "jekyll", "~> 4.2"
+gem "jekyll", "~> 4.2.2"
+
+gem "webrick", "~> 1.8"
 
 gem "jekyll-theme-minimal", "~> 0.2.0"
--- flake.nix
+++ flake.nix
@@ -1,13 +1,16 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
 
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
       pkgs = import nixpkgs { inherit system; };
 
+      ruby = pkgs.ruby_3_1;
+
       env = pkgs.bundlerEnv {
         name = "your-package";
-        inherit (pkgs) ruby;
+        inherit ruby;
         gemfile = ./Gemfile;
         lockfile = ./Gemfile.lock;
         gemset = ./gemset.nix;
@@ -30,7 +33,7 @@
           shellNix = import ./shell.nix { inherit pkgs; };
           jekyll = pkgs.mkShell {
             name = "jekyll-env";
-            packages = [env pkgs.bundler pkgs.ruby];
+            packages = [env pkgs.bundler ruby];
           };
           default = jekyll;
         };
